07218054ad2ef6493a34319c4e6e4522fc2269b6
jcat-github <117458947+jcat-github@users.noreply.github.com>
2022-12-18 21:10:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-e
+import requests as r
+import json
+api = json.loads(r.get("https://randomuser.me/api").text)
+name = api['results'][0]['name']['title']+" "+api['results'][0]['name']['first']
+print(name)
+import urllib.request
+
+image_url = api['results'][0]['picture']['large']
+
+urllib.request.urlretrieve(image_url, 'images/local_image.jpg')
+from flask import Flask
+app = Flask('app', static_folder="images/")
+
+@app.route('/')
+def hello_world():
+  return f'<img src="{image_url}"> <h2>{name}</h2>'
+
+app.run(host='0.0.0.0', port=8080)
+
